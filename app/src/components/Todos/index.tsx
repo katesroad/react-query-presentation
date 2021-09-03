9bde80c0bb97de1842da0b76e453fd5829bac8da
Kate <test@gmail.com>
2021-09-03 08:10:35 -0400

    Added no data

--- app/src/components/Todos/index.tsx
+++ app/src/components/Todos/index.tsx
@@ -1,64 +1,76 @@
-import { Wrapper } from 'components/Todos/styles';
+import { Wrapper } from "components/Todos/styles";
 import { useGetTodos } from "hooks/queries/todos.hooks";
 import * as React from "react";
 import { useTable } from "react-table";
 import { columns } from "./columns";
 
 interface TodosProps {
-	user: string;
+  user: string;
 }
 
 const Todos: React.FC<TodosProps> = ({ user }) => {
-	const { status, data: todos } = useGetTodos(user);
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-		useTable({
-			columns,
-			data: todos ?? [],
-		});
+  const { status, data: todos } = useGetTodos(user);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data: todos ?? [],
+    });
 
-	const content = status === 'success' ?
-		(
-			<table
-				{...getTableProps()}
-				className="table table-bordered table-striped table-hover table-sm"
-			>
-				<thead>
-					{headerGroups.map((headerGroup) => (
-						<tr {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map((column) => (
-								<th {...column.getHeaderProps()}>
-									{column.render("Header")}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody {...getTableBodyProps()}>
-					{rows.map((row, i) => {
-						prepareRow(row);
-						return (
-							<tr {...row.getRowProps()}>
-								{row.cells.map((cell) => {
-									return (
-										<td {...cell.getCellProps()}>
-											{cell.render("Cell")}
-										</td>
-									);
-								})}
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>) : (
-			['loading', 'idel'].includes(status) ? <p>Loading...</p> : <p>Failed to load data</p>
-		)
+  const content =
+    status === "success" ? (
+      <div>
+        <table
+          {...getTableProps()}
+          className="table table-bordered table-striped table-hover table-sm"
+        >
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        {!todos.length && (
+          <div className="no-data card">
+            <h4>No data</h4>
+          </div>
+        )}
+      </div>
+    ) : ["loading", "idel"].includes(status) ? (
+      <div className="no-data card">
+        <p>Loading...</p>
+      </div>
+    ) : (
+      <div className="no-data card">
+        <p>Loading...</p>
+      </div>
+    );
 
-	return (
-		<Wrapper>
-			<h4>Todo List</h4>
-			{user ? content : <p>No user is selected</p>}
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <h4>Todo List</h4>
+      {user ? content : <p>No user is selected</p>}
+    </Wrapper>
+  );
 };
 
 export default Todos;
