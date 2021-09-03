f2c0ee72fdf227ba52a4b9acd5e24b4aa527a056
Kate <test@gmail.com>
2021-09-03 08:05:05 -0400

    Added toast

--- app/src/context/index.tsx
+++ app/src/context/index.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     // configure the default stale, cacheTime
     queries: {
       cacheTime: 30 * 60 * 1000,
-      staleTime: 30 * 60 * 1000
-    }
-  }
-})
+      staleTime: 30 * 60 * 1000,
+    },
+  },
+});
 
-const AppProviders: React.FC = ({ children }) =>
-(<QueryClientProvider client={queryClient}>
-  <Router>
-    <Switch>{children}</Switch>
-  </Router>
-</QueryClientProvider>);
-
+const AppProviders: React.FC = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <ToastContainer autoClose={2000} />
+    <Router>
+      <Switch>{children}</Switch>
+    </Router>
+  </QueryClientProvider>
+);
 
 export default AppProviders;
--- app/src/hooks/queries/todos.hooks.ts
+++ app/src/hooks/queries/todos.hooks.ts
@@ -1,60 +1,67 @@
 import { getTodosKeys } from "hooks/queries/query-key";
 import { useMutation, useQuery, useQueryClient } from "react-query";
+import { toast } from "react-toastify";
 import {
-	createTodo, deleteTodo, getTodo, getTodos, updateTodo
+  createTodo,
+  deleteTodo,
+  getTodo,
+  getTodos,
+  updateTodo,
 } from "services/todos.api";
 import { ICreateTodo, ITodo } from "types/todo";
 import { getToDoKey } from "./query-key";
 
 export function useGetTodos(user: string) {
-	const queryKey = getTodosKeys(user);
-	const queryFn = () => getTodos(user);
-	const conf = {
-		enabled: !!user,
-		retry: 1,
-	};
+  const queryKey = getTodosKeys(user);
+  const queryFn = () => getTodos(user);
+  const conf = {
+    enabled: !!user,
+    retry: 1,
+  };
 
-	return useQuery(queryKey, queryFn, conf);
+  return useQuery(queryKey, queryFn, conf);
 }
 
 export function useCreateTodo(user: string) {
-	const queryFn = (data: ICreateTodo) => createTodo(data);
-	const queryKey = getTodosKeys(user);
+  const queryFn = (data: ICreateTodo) => createTodo(data);
+  const queryKey = getTodosKeys(user);
 
-	const queryClient = useQueryClient();
-	const conf = {
-		enabled: !!user,
-		retry: 1,
-		onSuccess: () => queryClient.prefetchQuery(queryKey),
-		onError: () => alert("Create todo failed"),
-	};
+  const queryClient = useQueryClient();
+  const conf = {
+    enabled: !!user,
+    retry: 1,
+    onSuccess: () => queryClient.prefetchQuery(queryKey),
+    onError: () => {
+      toast.error("Create to do failed");
+    },
+  };
 
-	return useMutation(queryKey, queryFn, conf);
+  return useMutation(queryKey, queryFn, conf);
 }
 
 export function useGetTodo(id?: string) {
-	const queryFn = () => getTodo(id ?? '');
-	const queryKey = getToDoKey(id ?? '');
+  const queryFn = () => getTodo(id ?? "");
+  const queryKey = getToDoKey(id ?? "");
 
-	return useQuery(queryKey, queryFn, { enabled: !!id });
+  return useQuery(queryKey, queryFn, { enabled: !!id });
 }
 
 export function useUpdateTodo(todoUser: string) {
-	const queryClient = useQueryClient();
-	const queryFn = (newData: ITodo) => updateTodo(newData);
-	const queryKey = getTodosKeys(todoUser);
+  const queryClient = useQueryClient();
+  const queryFn = (newData: ITodo) => updateTodo(newData);
+  const queryKey = getTodosKeys(todoUser);
 
-	return useMutation(queryFn, {
-		onSuccess: () => queryClient.invalidateQueries(queryKey),
-	});
+  return useMutation(queryFn, {
+    onSuccess: () => queryClient.invalidateQueries(queryKey),
+  });
 }
 
 export function useDeleteTodo(todoUser: string) {
-	const queryFn = (id: string) => deleteTodo(id);
-	const queryClient = useQueryClient();
-	const queryKey = getTodosKeys(todoUser);
+  const queryFn = (id: string) => deleteTodo(id);
+  const queryClient = useQueryClient();
+  const queryKey = getTodosKeys(todoUser);
 
-	return useMutation(queryFn, {
-		onSuccess: () => queryClient.invalidateQueries(queryKey),
-	});
+  return useMutation(queryFn, {
+    onSuccess: () => queryClient.invalidateQueries(queryKey),
+  });
 }
